ea8e0280ef2c278b31b60f3272611ebaf90fa522
JustSenoko <just.senoko@gmail.com>
2019-09-20 09:12:00 +0300

    EventBus, принимающий данные от двух Observable и передающий эти данные двум подписчикам

--- app/src/main/java/com/example/lesson2_2/MainActivity.java
+++ app/src/main/java/com/example/lesson2_2/MainActivity.java
@@ -2,13 +2,126 @@ package com.example.lesson2_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1;
+    EditText number2;
+    TextView text1;
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+
+        ObservableOnSubscribe<String> observableOnSubscribe1 = new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) {
+                number1.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        emitter.onNext(charSequence.toString());
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+            }
+        };
+
+        ObservableOnSubscribe<String> observableOnSubscribe2 = new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) {
+                number2.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        emitter.onNext(charSequence.toString());
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+            }
+        };
+
+        Observable<String> observable1 = Observable.create(observableOnSubscribe1);
+        Observable<String> observable2 = Observable.create(observableOnSubscribe2);
+
+        PublishSubject<String> subject = PublishSubject.create();
+
+        observable1.subscribe(subject);
+        observable2.subscribe(subject);
+
+        Observer<String> observer1 = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+
+            @Override
+            public void onNext(String s) {
+                text1.setText(s);
+            }
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
+
+        Observer<String> observer2 = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+
+            @Override
+            public void onNext(String s) {
+                text2.setText(s);
+            }
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
+
+        subject.safeSubscribe(observer1);
+        subject.safeSubscribe(observer2);
     }
 }
